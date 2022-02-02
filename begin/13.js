c6531a5d46c3544b1ed14c986a547985d60d496c
NN <nematulloturanov9@gmail.com>
2022-02-02 19:09:28 +0500

    I added fail 14

---
+++ begin/13.js
@@ -0,0 +1,8 @@
+const R1 = 10;
+const R2 = 6;
+const S1 = Math.PI * (Math.pow(R1, 2));
+const S2 = Math.PI * (Math.pow(R2, 2));;
+const S3 = S1-S2;
+console.log (S1);
+console.log (S2);
+console.log (S3);
