c7c90abf91cc54fa22b7c1086c7919f04293826e
Sobirjon Khodijonov <57181321+skhodijonov@users.noreply.github.com>
2022-02-11 18:47:30 +0500

    minor refactor if30

--- IF/30.js
+++ IF/30.js
@@ -1,63 +1,27 @@
 const g = parseInt(prompt());
 
-if(g===0){
-    console.log("Нулевое число.");
-}
-else if (g>0 && g<1000){
-        console.log("Положительное и ");
-    if(g<10){
-            console.log("Однозначное");
-        if(g%2===0){
-            console.log("Четное");
+if (g === 0) {
+    console.log('Нулевое число.');
+} else if (g > 1000) {
+    console.log('Значение является больше 1000!');
+} else {
+    if (g > 0) {
+        console.log('Положительное и ');
+    } else if (g < 0) {
+        console.log('Отрицательное и ');
     }
-        else{
-            console.log("Нечетное");
-    }}
-    if(g<100){
-            console.log("Дузначное");
-        if(g%2===0){
-            console.log("Четное");
+    let a = g * -1;
+    if (a < 10) {
+        console.log('Однозначное');
     }
-        else{
-            console.log("Нечетное");
-    }}
-    else{
-            console.log("Трехзначное")
-        if(g%2===0){
-            console.log("Четное");
+    if (a < 100) {
+        console.log('Дузначное');
+    } else {
+        console.log('Трехзначное');
     }
-        else{
-            console.log("Нечетное");
-    }}
-}
-
-else if (g<0 && g<1000){
-        console.log("Отрицательное и ");
-        let a = g* -1;
-    if(a<10){
-            console.log("Однозначное");
-        if(a%2===0){
-            console.log("Четное");
-}
-        else{
-            console.log("Нечетное");
-}}
-    if(a<100){
-            console.log("Дузначное");
-        if(a%2===0){
-            console.log("Четное");
-}
-        else{
-            console.log("Нечетное");
-}}
-    else{
-            console.log("Трехзначное");
-         if(a%2===0){
-            console.log("Четное");
-}
-        else{
-            console.log("Нечетное");
-}}}
-else{
-    console.log("Значение является больше 1000!");
+    if (a % 2 === 0) {
+        console.log('Четное');
+    } else {
+        console.log('Нечетное');
+    }
 }
