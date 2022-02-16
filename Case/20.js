33e42654e9c71a0bccdd7c6e958ceff9b1235ac3
NN <nematulloturanov9@gmail.com>
2022-02-16 21:39:52 +0500

    Ended all execises on level Case

---
+++ Case/20.js
@@ -0,0 +1,54 @@
+const D = parseInt(prompt());
+const M = parseInt(prompt());
+  
+switch (M) {
+    case 1:
+        if (D < 20) { console.log("Козерог!"); }
+        else { console.log("Водолей!"); }
+break;
+    case 2:
+        if (D < 19) { console.log("Водолей!"); }
+        else { console.log("Рыбы!"); }
+break;
+    case 3:
+        if (D < 21) { console.log("Рыбы!"); }
+        else { console.log("Овен!"); }
+break;
+    case 4:
+        if (D < 20) { console.log("Овен!"); }
+        else { console.log(" Телец!"); }
+break;
+    case 5:
+        if (D < 21) { console.log(" Телец!"); }
+        else { console.log("Близнецы!"); }
+break;
+    case 6:
+        if (D < 22) { console.log("Близнецы!"); }
+        else { console.log("Рак!"); }
+break;
+    case 7:
+        if (D < 23) { console.log("Рак!"); }
+        else { console.log("Лев!"); }
+break;
+    case 8:
+        if (D < 23) { console.log("Лев!"); }
+        else { console.log("Дева!"); }
+break;
+    case 9:
+        if (D < 23) { console.log("Дева!"); }
+        else { console.log("Весы!"); }
+break;
+    case 10:
+        if (D < 23) { console.log("Весы!"); }
+        else { console.log("Скорпион!"); }
+break;
+    case 11:
+        if (D < 23) { console.log("Скорпион!"); }
+        else { console.log("Стрелец!"); }
+break;
+    case 12:
+        if (D < 22) { console.log("Стрелец!"); }
+        else { console.log("Козерог!"); }
+break;
+
+}
